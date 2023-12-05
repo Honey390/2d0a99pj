bdda3e9574c4d209fe5bafc35e0189a3057e99d7
honey390 <happygirl161019@gmail.com>
2023-12-05 11:43:20 +0630

    todofinish

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+       
+        'asul': ['Asul', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
